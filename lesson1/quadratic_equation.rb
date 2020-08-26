6e0bad5208a4c7f7350e55848c6c977ffbd92b74
yerlanmad <studyworld.m@gmail.com>
2020-08-26 12:46:19 +0600

    Correction after review

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -11,9 +11,13 @@ d = b ** 2 - 4 * a * c
 
 if a == 0
   puts 'The equation is linear'
-elsif d >= 0
-  x1 = (-b + Math.sqrt(d)) / 2 * a
-  x2 = (-b - Math.sqrt(d)) / 2 * a
+elsif d == 0
+  x = -b / 2 * a
+  puts "D = #{d}, there is only one root x = #{x}"
+elsif d > 0
+  discriminant_sqrt = Math.sqrt(d)
+  x1 = (-b + discriminant_sqrt) / 2 * a
+  x2 = (-b - discriminant_sqrt) / 2 * a
   puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
 else
   puts "D = #{d}, there is no roots"
